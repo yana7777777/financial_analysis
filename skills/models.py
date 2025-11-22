781c0d6c8b48b41b09b1cf990268a680c10d0ff0
yana7777777 <yana_77_70@mail.ru>
2025-11-22 13:15:07 +0400

    added new_project

--- skills/models.py
+++ skills/models.py
@@ -5,6 +5,7 @@ class Skills(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField('skills/images/')
+    pdf_file = models.FileField(upload_to='skills/pdf/', blank=True, null=True)
     url = models.URLField(blank=True)
 
     def __str__(self):
